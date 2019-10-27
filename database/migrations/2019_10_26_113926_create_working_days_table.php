0a8900fab21c7074bdc7ad8d80d129b2734490e2
須川誠 <>
2019-10-27 09:01:45 +0900

    working_daysテーブル仮設置

---
+++ database/migrations/2019_10_26_113926_create_working_days_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkingDaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {   
+        
+        Schema::create('working_days', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->datetime('start_time');
+            $table->datetime('end_time');
+            $table->datetime('today');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('working_days');
+    }
+}
