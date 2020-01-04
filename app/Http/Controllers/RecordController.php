45af33913b30332991c2f1fe0a65fe452fa62fa1
須川誠 <>
2020-01-04 13:48:16 +0900

    コンフリクト解消

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -1,55 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-<<<<<<< HEAD
-
-use Illuminate\Http\Request;
-use App\Models\Working_days;
-// お試し↓
-use Auth;
-// お試し↓
-use App\stamp;
-// お試し↓
-use Carbon\Carbon;
-
-
-class RecordController extends Controller {
-    public function model()
-    {
-      // Working_daysモデルのインスタンス化
-      $md = new Working_days();
-      // データ取得
-      $data = $md->getData();
-  
-      // ビューを返す
-      return view('record', ['data' => $data]);
-    }
-
-    
-
-
-
-
-
-
-
-
-    // public function record(){
-
-    //     //月初から月末の間をforeach
-    //     $data = ["りんご","概ね赤","2019年"];
-    //     // recordと$dataを持ってbladeに行く事ができる↓
-    //     return view('record', ['data' => $data]);
-    // }
-
-    // public function record(){
-    // $holidays = Yasumi::create('Japan', '2018', 'ja_JP');
-    // return view('record', $holidays); 
-    // }
- 
-    
-
-=======
 use App\Models\Working_days;
 use App\Models\view_working_days;
 use App\Models\calendar;
@@ -84,5 +35,4 @@ class RecordController extends Controller {
     return view('record',compact('calendar', 'working_days', 'yasumi'));
       
   }
->>>>>>> a573a90f11b0f00842e3701402bb2ac6ec92f4e1
 }
