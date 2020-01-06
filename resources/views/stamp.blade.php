b86e8f4f48fd908b17813862355cb1d6392f28b5
須川誠 <>
2020-01-06 22:23:08 +0900

    stampの時計Script削除

--- resources/views/stamp.blade.php
+++ resources/views/stamp.blade.php
@@ -59,59 +59,6 @@
 <!-- script領域 -->
 @section('script')
 <script>
-    	// 曜日の取得↓
-    	// var now =new Date();
-    	// var youbi = now.getDay();
-    	// var weekday = ["（日）","（月）","（火）","（水）","（木）","（金）","（土）"];
-		// document.getElementById('youbi').textContent = weekday[youbi];
-
-		// 年月日取得の処理↓
-    	function set2digits(number) {
-		    if (number < 10) {
-		        return '0' + number;
-		    }
-		    return number;
-		}
-		function setClock() {
-		    var nowTime = new Date();
-		    var nowHour = set2digits(nowTime.getHours());
-		    var nowMin  = set2digits(nowTime.getMinutes());
-		    var nowSec  = set2digits(nowTime.getSeconds());
-		    var clock   = "<h1 class='size'>" + nowHour + ":" + nowMin + "</h1>";
-		    // document.getElementById("clock").innerHTML = clock;
-		}
-       setInterval('setClock()', 1000);
-
-
-// // reloadの基本的な使い方
-// function doReload() {
- 
-//  // reloadメソッドによりページをリロード
-//  window.location.reload();
-// }
-
-// window.addEventListener('load', function () {
-
-//  // ページ表示完了した3秒後にリロード
-//  setTimeout(doReload, 10000);
-// });
-
-
-// var score = 90;
-
-// if (score >= 80) {
-// document.write("合格です！おめでとうございます！");
-// }
-
-
-// window.onload = function () { // ページ読み込み時に実行する無名関数
-//     document.getElementById( "calendar" ).click();
-// }
-// window.onload = function () { // ページ読み込み時に実行する無名関数
-//     document.getElementById( "yasumi" ).click();
-// }
-
-
 window.onload = function()
 {
 Time = new Date();
@@ -122,10 +69,5 @@ setTimeout(function(){
 location.reload(true)
 }, Rest);
 }
-
-
-
-
-
 </script>
 @endsection
